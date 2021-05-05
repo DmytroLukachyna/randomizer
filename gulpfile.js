093c4d7d256df070597ea248a7808dfb2d66a9ef
Dmytro Lukachyna <web.development@ukr.net>
2021-05-05 22:52:33 +0300

    v1.0.3, Minor updates, internal fonts added

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,15 @@ const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 const minify = require('gulp-minify');
 
 gulp.task('server', function() {
-
     browserSync({
         server: {
             baseDir: "dist"
         }
     });
-
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
@@ -33,9 +30,7 @@ gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
-    gulp.watch("src/mailer/**/*").on('change', gulp.parallel('mailer'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
 
 gulp.task('html', function() {
@@ -65,17 +60,4 @@ gulp.task('fonts', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('images', function() {
-    return gulp.src("src/img/**/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('mailer', function() {
-    return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest("dist/mailer"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'images', 'mailer'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts'));
